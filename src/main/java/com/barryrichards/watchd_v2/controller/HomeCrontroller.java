d5e6411330c4c1208c80c14e831022baafbee89c
= <=>
2026-07-23 12:55:50 +0100

    added security config to permit all access temporarily and a homecontroller

---
+++ src/main/java/com/barryrichards/watchd_v2/controller/HomeCrontroller.java
@@ -0,0 +1,14 @@
+package com.barryrichards.watchd_v2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeCrontroller {
+    
+    @GetMapping("/")
+    public String getHome() {
+        return "Home";
+    }
+
+}
