e564f1f615314045654783d3eb95db5b8df0336b
= <=>
2026-07-23 11:15:45 +0100

    spring setup

---
+++ src/main/java/com/barryrichards/watchd_v2/WatchdV2Application.java
@@ -0,0 +1,13 @@
+package com.barryrichards.watchd_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchdV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchdV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/barryrichards/watchd_v2/WatchdV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barryrichards.watchd_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
