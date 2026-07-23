51c47bef4c9176e34aeadf8c637f2cbd25895b5e
= <=>
2026-07-23 12:33:46 +0100

    added service annotation

--- src/main/java/com/barryrichards/watchd_v2/service/AccountService.java
+++ src/main/java/com/barryrichards/watchd_v2/service/AccountService.java
@@ -2,12 +2,15 @@ package com.barryrichards.watchd_v2.service;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
 import com.barryrichards.watchd_v2.model.Account;
 import com.barryrichards.watchd_v2.repository.AccountRepository;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@Service
 public class AccountService {
     
     private AccountRepository accountRepository;
