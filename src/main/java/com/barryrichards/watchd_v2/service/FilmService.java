cbd84572aea7264c4f3587897386362306fe30cf
= <=>
2026-07-24 16:36:27 +0100

    added findOrCreateByTmdbId method

--- src/main/java/com/barryrichards/watchd_v2/service/FilmService.java
+++ src/main/java/com/barryrichards/watchd_v2/service/FilmService.java
@@ -1,9 +1,12 @@
 package com.barryrichards.watchd_v2.service;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.barryrichards.watchd_v2.client.TmdbClient;
+import com.barryrichards.watchd_v2.dto.TmdbFilmResult;
 import com.barryrichards.watchd_v2.model.Film;
 import com.barryrichards.watchd_v2.repository.FilmRepository;
 
@@ -14,8 +17,28 @@ import lombok.AllArgsConstructor;
 public class FilmService {
 
     private FilmRepository filmRepository;
+    private TmdbClient tmdbClient;
     
     public Film findById(UUID id) {
         return filmRepository.findById(id).orElse(null);
+    }
+
+    public Film findOrCreateByTmdbId(String tmdbId) {
+        try {
+            if (filmRepository.existsByTmdbId(tmdbId)) {
+                return filmRepository.getByTmdbId(tmdbId).get();
+            } else {
+                TmdbFilmResult tmdbFilmResult = tmdbClient.getByTmdbId(tmdbId);
+                String title = tmdbFilmResult.title();
+                String posterUrl = tmdbFilmResult.posterPath();
+                String synopsis = tmdbFilmResult.overview();
+                LocalDate releaseDate = tmdbFilmResult.releaseDate();
+                Film film = new Film(tmdbId, title, posterUrl, synopsis, releaseDate);
+                return filmRepository.save(film);
+            }
+        } catch (Exception e) {
+            System.out.println(">>>>>>>>>> " + e);
+            return null;
+        }
     }
 }
