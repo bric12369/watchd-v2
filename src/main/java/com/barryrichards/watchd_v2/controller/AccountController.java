80ee5ffc3f9691b9bb743d085dc2345638ea7cd6
= <=>
2026-08-04 10:37:07 +0100

    added signUp capability

--- src/main/java/com/barryrichards/watchd_v2/controller/AccountController.java
+++ src/main/java/com/barryrichards/watchd_v2/controller/AccountController.java
@@ -4,12 +4,17 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.barryrichards.watchd_v2.dto.LoginRequest;
+import com.barryrichards.watchd_v2.dto.SignUpRequest;
 import com.barryrichards.watchd_v2.model.Account;
 import com.barryrichards.watchd_v2.service.AccountService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -22,5 +27,10 @@ public class AccountController {
     @GetMapping("/{id}")
     public Account getAccountById(@PathVariable UUID id) {
         return accountService.findById(id);
+    }
+
+    @PostMapping("/sign-up")
+    public Account signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+        return accountService.createAccount(signUpRequest);
     }
 }
